05362f1fef37707a667a5510139b57e93c5243ff
taofeek ajibade <cleargoal.ng@gmail.com>
2022-06-24 22:55:40 +0100

    added js code

--- name.js
+++ name.js
@@ -1,1 +1,9 @@
 document.write("This is Taofeek");
+
+Name = document.querySelector(".name").innerHTML = "My name is Taofeek Ajibade";
+
+Height = document.querySelector(".height").innerHTML = "I am 6ft tall.";
+
+Country = document.querySelector(".country").innerHTML = "I am a citizen of Nigeria";
+
+alert(Name, Height, Country);
